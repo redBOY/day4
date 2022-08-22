aeaf9239c4781decdb4d4d8e7f92f3fb02a87db2
redBOY <43438363+redBOY@users.noreply.github.com>
2022-08-22 11:23:09 +0530

    Welcome to Snake and Ladder

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -3,7 +3,7 @@ package main.main.day4;
 public class Snake_And_Ladder {
 
         public static void main(String[] args) {
-            System.out.println("Welcome to Snake Ladder Game");
+            System.out.println("Welcome to Snake and Ladder Game");
         }
 
 
